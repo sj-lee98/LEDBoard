683461f03c47ffcb75125cbca40fc08512943f55
sj-lee98 <denny00123@naver.com>
2021-09-17 13:57:41 +0900

    + 1차 최종본 완성 [Navigation controller, 화면간 전환, Life cycle, 화면간 데이터 전달 실습]

--- LEDBoard/SettingViewController.swift
+++ LEDBoard/SettingViewController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+protocol LEDBoardSettingDelegate: AnyObject {
+    func changedSetting(text: String?, textColor: UIColor, backgroundColor: UIColor)
+}
+
 class SettingViewController: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
@@ -17,32 +21,61 @@ class SettingViewController: UIViewController {
     @IBOutlet weak var blueButton: UIButton!
     @IBOutlet weak var orangeButton: UIButton!
     
+    weak var delegate: LEDBoardSettingDelegate?
+    var ledText: String?
+    //색상 초기화 글자는 노란색, 배경은 검정색
+    var textColor: UIColor = .yellow
+    var backgroundColor: UIColor = .black
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.configureView()
+    }
+    
+    private func configureView() {
+        if let ledText = self.ledText {
+            self.textField.text = ledText
+        }
+        self.changeTextColor(color: self.textColor)
+        self.changeBackgroundColor(color: self.backgroundColor)
     }
     
     @IBAction func tapTextColorButton(_ sender: UIButton) {
         if sender == self.yellowButton {
             self.changeTextColor(color: .yellow)
+            self.textColor = .yellow
         } else if sender == self.purpleButton {
             self.changeTextColor(color: .purple)
+            self.textColor = .purple
         } else {
             self.changeTextColor(color: .green)
+            self.textColor = .green
         }
     }
     
     @IBAction func tapBackgroundColorButton(_ sender: UIButton) {
         if sender == self.blackButton {
             self.changeBackgroundColor(color: .black)
+            self.backgroundColor = .black
         } else if sender == self.blueButton {
             self.changeBackgroundColor(color: .blue)
+            self.backgroundColor = .blue
         } else {
             self.changeBackgroundColor(color: .orange)
+            self.backgroundColor = .orange
         }
     }
     
     @IBAction func tapSaveButton(_ sender: UIButton) {
+        //delegate 이기 때문에 모든 파일로 데이터 넘길 수 있음 (버튼 색상 및 배경 색상 정보 전송)
+        self.delegate?.changedSetting(
+            text: textField.text,
+            textColor: self.textColor,
+            backgroundColor: self.backgroundColor
+        )
+        
+        //이전화면으로 복귀
+        self.navigationController?.popViewController(animated: true)
     }
     
     private func changeTextColor(color: UIColor) {
@@ -53,7 +86,6 @@ class SettingViewController: UIViewController {
     }
     
     private func changeBackgroundColor(color: UIColor) {
-        //노랑버튼의 알파값이 UICOLOR의 노란색의 알파값이면 알파값을 1로 , 아니면 0.2로 세팅
         self.blackButton.alpha = color == UIColor.black ? 1 : 0.2
         self.blueButton.alpha = color == UIColor.blue ? 1 : 0.2
         self.orangeButton.alpha = color == UIColor.orange ? 1 : 0.2
--- LEDBoard/ViewController.swift
+++ LEDBoard/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LEDBoardSettingDelegate {
 
     @IBOutlet weak var contentsLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.contentsLabel.textColor = .yellow
+    }
+    
+    //segue way로 화면전환을 구현 하였기 때문에 prepare 메서드를 override 해줘야함
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let settingViewController = segue.destination as? SettingViewController {
+            settingViewController.delegate = self
+            settingViewController.ledText = self.contentsLabel.text
+            settingViewController.textColor = self.contentsLabel.textColor
+            settingViewController.backgroundColor = self.view.backgroundColor ?? .black
+        }
+    }
+    
+    func changedSetting(text: String?, textColor: UIColor, backgroundColor: UIColor) {
+        if let text = text { //text가 String? 이기 때문에 옵셔널 바인딩 사용
+            self.contentsLabel.text = text
+        }
+        self.contentsLabel.textColor = textColor
+        self.view.backgroundColor = backgroundColor
     }
 
 
